6d26a7d1179ebe56f9610e1f0ccf758bacdd37a8
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2025-11-24 15:02:54 +0000

    added an aditional test

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -11,6 +11,7 @@ public class PositionTest {
         this.position = new Position(10,10);
     }
 
+    /** test the getters */
     @Test
     public void testGetX(){
         assertEquals(10,position.getX());
@@ -21,19 +22,27 @@ public class PositionTest {
         assertEquals(10,position.getY());
     }
 
+    /** test if the equal objects equal true*/
     @Test
     public void testEqualsObject(){
         assertTrue(position.equals(new Position(10,10)));
     }
 
+    /** test if null objects equal false */
     @Test
     public void testEqualsNullObject(){
         assertFalse(position.equals(null));
     }
 
+    /** test if two position objects have same positions */
     @Test
     public void testEquals(){
         Position p1 = new Position(10,10);
-        assertTrue(p1.equals(p1));
+        assertTrue(position.equals(p1));
+
+        Position p2 = new Position(10,8);
+        assertFalse(position.equals(p2));
     }
+
+
 }
